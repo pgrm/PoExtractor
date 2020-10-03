0bb5778f7098754e43febc9748c0333f234f0e7f
Peter Grman <peter.grman@gmail.com>
2020-10-03 16:55:45 +0200

    simplify CompileViews

--- PoExtractor.Razor/ViewCompiler.cs
+++ PoExtractor.Razor/ViewCompiler.cs
@@ -8,31 +8,17 @@ namespace PoExtractor.Razor
 {
     public static class ViewCompiler
     {
-        public static IList<RazorPageGeneratorResult> CompileViews(string projectDirectory)
+        public static IEnumerable<RazorPageGeneratorResult> CompileViews(string projectDirectory)
         {
             var projectEngine = CreateProjectEngine("PoExtractor.GeneratedCode", projectDirectory);
 
-            var results = new List<RazorPageGeneratorResult>();
-
-            var allDirectories = Directory.EnumerateDirectories(projectDirectory, "*", SearchOption.AllDirectories)
-                .Concat(new []{ projectDirectory })
-                .Distinct()
-                .OrderBy(dirName => dirName);
-            foreach (var dir in allDirectories)
+            foreach (var item in projectEngine.FileSystem.EnumerateItems(projectDirectory).OrderBy(rzrProjItem => rzrProjItem.FileName))
             {
-                var dirPath = dir.Substring(projectDirectory.Length).Replace('\\', '/');
-                var razorFiles = projectEngine.FileSystem.EnumerateItems(dirPath).OrderBy(rzrProjItem => rzrProjItem.FileName);
-
-                foreach (var item in razorFiles.Where(o => o.Extension == ".cshtml" || o.Extension == ".razor"))
-                {
-                    results.Add(GenerateCodeFile(projectEngine, item));
-                }
+                yield return GenerateCodeFile(projectEngine, item);
             }
-
-            return results;
         }
 
-        public static RazorProjectEngine CreateProjectEngine(string rootNamespace, string projectDirectory)
+        private static RazorProjectEngine CreateProjectEngine(string rootNamespace, string projectDirectory)
         {
             var fileSystem = RazorProjectFileSystem.Create(projectDirectory);
             var projectEngine = RazorProjectEngine.Create(RazorConfiguration.Default, fileSystem, builder =>
@@ -53,7 +39,7 @@ namespace PoExtractor.Razor
             return projectEngine;
         }
 
-        public static RazorPageGeneratorResult GenerateCodeFile(RazorProjectEngine projectEngine, RazorProjectItem projectItem)
+        private static RazorPageGeneratorResult GenerateCodeFile(RazorProjectEngine projectEngine, RazorProjectItem projectItem)
         {
             var codeDocument = projectEngine.Process(projectItem);
             var cSharpDocument = codeDocument.GetCSharpDocument();
